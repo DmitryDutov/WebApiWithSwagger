77f77e8945c477b3082eb1fb2422c81223a9423b
DmitryDutov <dmitry_dutov@inbox.ru>
2022-10-29 16:14:51 +0300

    Create new Eqp

--- WebApiWithSwagger/Controllers/SettingsController.cs
+++ WebApiWithSwagger/Controllers/SettingsController.cs
@@ -29,34 +29,33 @@ namespace WebApiWithSwagger.Controllers
             return serialize;
         }
 
-        [HttpPost("add")] //POST -> http://localhost:5229/api/add
+        [HttpPost("add")] //POST -> "https://localhost:5163/api/Settings/add/NewEqp;NewEqp.txt;127.0.0.3;8888;false;unicode;1;[a-z]"
         public IActionResult Add([FromBody] string value)
         {
             var settings = Settings.SettingsObject;
-            var eqpGuid = settings.Eqps.Select(x => x.EqpGuid).Max();
-            var line = Convert.ToString(eqpGuid);
-            var num = Convert.ToInt64(line.Replace("-", string.Empty));
+            var stringGuid = settings.Eqps.Select(x => x.EqpGuid).Max().ToString().Replace("-", string.Empty);
+            var num = Convert.ToInt64(stringGuid);
             num++;
-            var format = string.Format($"{num:00000000-0000-0000-0000-000000000000}"); //00000000-0000-0000-0000-000000000012
+            var format = string.Format($"{num:00000000-0000-0000-0000-000000000000}");
             var nextGuid = Guid.Parse(format);
 
-            //var deserialize = JsonConvert.DeserializeObject<Rootobject>(File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "appsettings.json")));
-            var newEqp = JsonConvert.DeserializeObject<Eqp>(value);
+            //Считываем строку, на основе которой будем формироваться новая единица оборудования
+            var newEqp = CreateEqp(value);
+
             if (newEqp != null)
             {
                 newEqp.EqpGuid = nextGuid;
 
                 var listEquips = settings.Eqps; //получаем список оборудования
-                listEquips.Add(newEqp); //добавляем новое
+                listEquips.Add(newEqp);         //добавляем новое
 
-                //Settings.SaveSettings(Path.Combine(AppContext.BaseDirectory, "appsettings.json"));
+                settings.Eqps = listEquips;
                 Settings.SaveSettings(@"C:\Test\Severstal.DeviceMonitoring\Settings\appsettings.json");
                 return Ok();
             }
 
             return NotFound();
         }
-
 
         // PUT api/<SettingsController>/5
         [HttpPut("{id}")]
@@ -68,6 +67,46 @@ namespace WebApiWithSwagger.Controllers
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
+        }
+
+        private Eqp? CreateEqp(string value)
+        {
+            var parameters = value.Split(';').ToList();
+            switch (parameters.Count)
+            {
+                case 8:
+                    {
+                        var newEqp = new Eqp()
+                        {
+                            Name = parameters[0],
+                            Path = parameters[1],
+                            Address = parameters[2],
+                            Port = Convert.ToInt32(parameters[3]),
+                            Active = Convert.ToBoolean(parameters[4]),
+                            Encoding = parameters[5],
+                            Interval = Convert.ToInt32(parameters[6]),
+                            Mask = parameters[7],
+                        };
+
+                        return newEqp;
+                    }
+                case 6:
+                    {
+                        var newEqp = new Eqp()
+                        {
+                            Name = parameters[0],
+                            Path = parameters[1],
+                            Address = parameters[2],
+                            Port = Convert.ToInt32(parameters[3]),
+                            Active = Convert.ToBoolean(parameters[4]),
+                            Encoding = parameters[5],
+                        };
+
+                        return newEqp;
+                    }
+            }
+
+            return null;
         }
     }
 }
